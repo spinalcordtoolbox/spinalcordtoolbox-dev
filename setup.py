d7546fa39801d3d90b1963ec855dff3f0ed9928d
Nick Guenther <nguenthe@uwaterloo.ca>
2021-11-22 13:39:16 -0500

    Parallel python3.6 env

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     packages=find_packages(exclude=['.git', 'data', 'dev', 'dev.*',
                                     'install', 'testing']),
     include_package_data=True,
-    python_requires="==3.7.*",
+    python_requires="==3.6.*",
     extras_require={
         'docs': [
             'sphinxcontrib-programoutput',
