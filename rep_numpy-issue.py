b7dea3f2cbed0fd1a0bbcb19c417ecf5845cf551
Joshua Newton <joshuacwnewton@gmail.com>
2021-12-10 13:08:15 -0500

    Add reproducibility script from numpy issue

---
+++ rep_numpy-issue.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+array1 = np.load('array1.npy')
+array2 = np.load('array2.npy')
+array_dot = array1.dot(array2)
+
+# Test against the "failing" dot product
+array_fail = np.load('result-fail.npy')
+print(f"Matches cached 'fail' array: {np.array_equal(array_dot, array_fail)}")
+
+# Test against the "passing" dot product
+array_pass = np.load('result-pass.npy')
+print(f"Matches cached 'pass' array: {np.array_equal(array_dot, array_pass)}")
+
+# On Xeon(R) Platinum 8370C, array_dot should match array_fail, so the tests will fail
+# On Xeon(R) Platinum 8171M, array_dot should match array_pass, so the tests will pass
